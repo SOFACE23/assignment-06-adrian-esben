5e534f5a6e7cb181d858314a6b15e32704726da7
SejDreng <adriananthony10@gmail.com>
2023-03-07 15:44:50 +0100

    push

--- src/main.cpp
+++ src/main.cpp
@@ -6,19 +6,5 @@
 
 int main(int argc, char **argv)
 {
-    std::vector<BYTE> readFile(const char* filename)
-{
-    // open the file:
-    std::streampos fileSize;
-    std::ifstream file(filename, std::ios::binary);
-
-    // get its size:
-    file.seekg(0, std::ios::end);
-    fileSize = file.tellg();
-    file.seekg(0, std::ios::beg);
-
-    // read the data:
-    std::vector<BYTE> fileData(fileSize);
-    file.read((char*) &fileData[0], fileSize);
-    return fileData;
+    return 0;
 }
