2e162968595834bf6f28f9d66126a364f47ba24c
SejDreng <adriananthony10@gmail.com>
2023-03-09 11:39:05 +0100

    færdig med kommentarer

--- src/daytime_client.cpp
+++ src/daytime_client.cpp
@@ -24,21 +24,21 @@ int main(int argc, char* argv[])
       return 1;
     }
 
-    boost::asio::io_context io_context;
+    boost::asio::io_context io_context; //creating an io_context object.
 
-    tcp::resolver resolver(io_context);
+    tcp::resolver resolver(io_context); //Creating a resolver object with the io_context variable as an attribute
     tcp::resolver::results_type endpoints =
-      resolver.resolve(argv[1], "daytime");
+      resolver.resolve(argv[1], "daytime"); //Defining the endpoints variable  with the resolve method, with the atrributes of the server we want to access, which in this case is the "daytime" service.
 
-    tcp::socket socket(io_context);
-    boost::asio::connect(socket, endpoints);
+    tcp::socket socket(io_context); // We create a socket to communicate with.
+    boost::asio::connect(socket, endpoints); // We connect to the endpoints, which was defined above, usingthe socket we just created. 
 
     while(true)
     {
-      boost::array<char, 128> buf;
+      boost::array<char, 128> buf; //Creating an array with the name buf size of 128 chars for our buffed message
       boost::system::error_code error; // Creating an error variable.
 
-      size_t len = socket.read_some(boost::asio::buffer(buf), error); // Using the read method to read the buffed string from the daytime server.
+      size_t len = socket.read_some(boost::asio::buffer(buf), error); // Using the read method to read the buffed string from the daytime server to the buf array.
 
       if (error == boost::asio::error::eof)
         break; // Connection closed cleanly by peer.
